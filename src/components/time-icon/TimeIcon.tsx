18e34cffb3014868499c4d6ee125cfc7436a3a19
Nafikov20 <snkr.nk95@gmail.com>
2023-09-02 15:24:42 +0300

    some edits

--- src/components/time-icon/TimeIcon.tsx
+++ src/components/time-icon/TimeIcon.tsx
@@ -1,6 +1,7 @@
+import '../../App.css'
 export const TimeIcon = () => {
   return (
-    <div className='absolute left-[-30px] top-[0px] h-[25px] w-[25px]'>
+    <div className='icon'>
       <svg height='100%' width='100%' xmlns='http://www.w3.org/2000/svg'>
         <path d='M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z' />
         <path d='M13 7h-2v6h6v-2h-4z' />
